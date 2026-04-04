68d4ce1799f5d4435bc5ee76589c880561835370
Pavel Vanecek <corkscreewe@gmail.com>
2026-04-04 15:40:36 +0900

    Add AnimationProgressProvider and manual animation control features

--- www/src/docs/apiCates.ts
+++ www/src/docs/apiCates.ts
@@ -105,6 +105,6 @@ export const apiCates = [
   },
   {
     name: 'animation',
-    items: ['matchByIndex', 'matchByDataKey'],
+    items: ['matchByIndex', 'matchByDataKey', 'AnimationProgressProvider', 'useAnimationProgress'],
   },
 ];
